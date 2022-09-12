861a367f968d99387e4e50498dc93cc8aeba85e4
Shahin Ansari <monazzam@protonmail.com>
2022-09-11 21:45:02 -0400

    added multiples of 3 and 5.

---
+++ src/main/java/MultiplesThreeAndFive.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiplesThreeAndFive {
+    public List<Integer> calculateMultiples(int m, int n, int limit) {
+        List<Integer> result = new ArrayList<>();
+        if (m == 0 || n == 0 || limit == 0) return result;
+        int index = 2;
+        if (m > n) {
+            while (m * index < limit) {
+                if (m * index % n == 0) result.add(m * m);
+                index++;
+            }
+        } else {
+            index = 2;
+            while (n * index < limit) {
+                if (n * index % m == 0) result.add(n * index);
+                index++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int m = 3;
+        int n = 5;
+        int limit = 1000;
+        MultiplesThreeAndFive multiplesThreeAndFive = new MultiplesThreeAndFive();
+        for(int i: multiplesThreeAndFive.calculateMultiples(m, n, limit)) System.out.print(" "+i);
+        System.out.println();
+    }
+}
