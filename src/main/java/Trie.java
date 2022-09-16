91a9e9e8d52d23895f15e82c0ca1efca722b898a
Shahin Ansari <monazzam@protonmail.com>
2022-09-16 10:58:32 -0400

    Working with Tries a bit. Started Trie api

--- src/main/java/Trie.java
+++ src/main/java/Trie.java
@@ -1,4 +1,8 @@
-public class Trie {
+import edu.princeton.cs.algs4.Queue;
+
+import javax.swing.plaf.PanelUI;
+// code from Sedgwik book
+public class Trie<Value> {
     private static int R = 256;//radix
     private Node root;
 
@@ -7,15 +11,53 @@ public class Trie {
         private Object val;
     }
 
-    private void size() {
-        for (char c = 0; c < R; c++) {
-            System.out.print(" " + c);
+    public Value get(String key) {
+        Node x = get(root, key, 0);
+        if (x == null) return null;
+        return (Value) x.val;
+    }
+
+    private Node get(Node x, String key, int d) {
+        if (x == null) return null;
+        if (d == key.length()) return x;
+        char c = key.charAt(d);
+        return get(x.next[c], key, d);
+    }
+
+    public void put(String key, Value value) {
+        root = put(root, key, value, 0);
+    }
+
+    private Node put(Node x, String key, Value value, int d) {
+        if (x == null) return new Node();
+        if (d == key.length()) {
+            x.val = value;
+            return x;
         }
+        char c = key.charAt(d);
+        x.next[c] = put(x.next[c], key, value, d + 1);
+        return x;
+    }
+
+    public Iterable<String> keys() {
+        return keysWithPrefix("");
+    }
+
+    public Iterable<String> keysWithPrefix(String pre) {
+        Queue<String> q = new Queue<>();
+        collect(get(root, pre, 0), pre, q);
+        return q;
+    }
+
+    private void collect(Node x, String pre, Queue<String> q) {
+        if (x == null) return;
+        if (x.val != null) q.enqueue(pre);
+        for (char c = 0; c < R; c++) collect(x.next[c], pre + c, q);
     }
 
     public static void main(String[] args) {
         Trie t = new Trie();
-        t.size();
+
     }
 
 }
